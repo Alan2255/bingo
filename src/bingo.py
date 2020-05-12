0d14ea9563d7a0f666b4b0564aa64d301d3c21a5
Alan <alan@example.com>
2020-05-11 22:26:53 -0300

    Elimino funcion carton_ejemplo

--- src/bingo.py
+++ src/bingo.py
@@ -6,13 +6,6 @@ import random
 # Los 0 representan celdas vacias.
 # Los numeros mayores que cero representan celdas ocupadas.
 
-def  carton_ejemplo():
-     carton = [
-       [7 ,10 ,0  ,0  ,41 ,0  ,0  ,72  ,80],
-       [0 ,12 ,25 ,0  ,48 ,52 ,0  ,78  ,0 ],
-       [9 ,0  ,29 ,33 ,0  ,0  ,63 ,0   ,90],
-     ]
-     return carton
 
 # Genera un talonario de bingo valido.
 def generador_bingo():
