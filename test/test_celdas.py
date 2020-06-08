b0f7701cd3ca7c87fb845a512b052a8589b4cea5
Alan <alan@example.com>
2020-06-08 15:16:56 -0300

    Configuro coveralls sobre bingo.py y agrego fors a los test

--- test/test_celdas.py
+++ test/test_celdas.py
@@ -7,16 +7,19 @@ from src.bingo import nomasde3
 
 # Verifico que cada columna no este vacia ni llena.
 def test_columnas():
+    for i in range(10):
       assert columnas(ejemplo()) == True
 
 
 # Verifico que cada 3 celdas consecutivas no haya mas de 3 ocupadas o vacias.
 def test_nomasde3():
+    for i in range(10):
       assert nomasde3(ejemplo()) == True
 
 
 # Verifico que cada fila tenga 5 celdas ocupadas.
 def test_fila():
+    for i in range(10):
       assert fila(ejemplo()) == True
 
 
