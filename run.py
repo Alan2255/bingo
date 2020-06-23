ed7b061c00fb6913a25d6797ff6b5ad8f4ace7e5
Alan <alan@example.com>
2020-06-23 19:08:04 -0300

    Agrego colores aleatorios por carga de pagina

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
+# Autor: Alan Hergenreder
+
+import random
 from src.bingo import generador_bingo
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    color = ['Gold','LawnGreen','OrangeRed','Teal','SaddleBrown','Indigo','HotPink','SeaGreen','DarkRed']
     t = generador_bingo()
-    return render_template("plantilla.html",talonario=t)
+    return render_template("plantilla.html",talonario=t,color=color[random.randrange(9)])
